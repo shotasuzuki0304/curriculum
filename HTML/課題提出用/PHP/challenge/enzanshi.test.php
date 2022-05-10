114db241fc8b5234659f875809d5f9d7676c4d08
shotasuzuki0304 <104036883+shotasuzuki0304@users.noreply.github.com>
2022-05-11 05:29:48 +0900

    suzuki
    課題

---
+++ HTML/課題提出用/PHP/challenge/enzanshi.test.php
@@ -0,0 +1,38 @@
+<?php
+$x = 5;
+
+$x++;
+echo $x;
+echo '<br>';
+
+$x--;
+echo $x;
+echo '<br>';
+?>
+
+<?php
+echo 1 + 1;
+// 改行は適宜入れた方が見やすくなります。
+echo '<br>';
+echo 10 - 1;
+echo '<br>';
+echo 2 * 2;
+echo '<br>';
+echo 10 / 5;
+echo '<br>';
+echo 5 % 3;
+echo '<br>';
+?>
+
+<?php
+$x = 10;
+// 単項演算子
+// $x = $x + 1
+$x++; 
+echo $x;
+echo '<br>';
+// $x = $x - 1
+$x--;
+echo $x;
+echo '<br>';
+?>
