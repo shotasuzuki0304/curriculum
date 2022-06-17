9dbd8007021da4537ab5bae841bc4a16ddf06183
shotasuzuki0304 <104036883+shotasuzuki0304@users.noreply.github.com>
2022-06-17 17:28:37 +0900

    鈴木
    訂正
    4章チェックテスト質問内容に関係するファイルです。
    
    ・参照場所
    /HTML/質問用/4-2
    
    よろしくお願いいたします。

---
+++ HTML/質問用/4-2/index.php
@@ -0,0 +1,38 @@
+<html lang="en">    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <imag src="4-2/1599315827_logo.png" />
+    <imag src="/Applications/MAMP/htdocs/LetsEngineer/curriculum/PHPjob(MANP)/4-2/1599315827_logo.png" />
+    <imag src="4-2/1599315827_logo.png"></imag>
+    <imag src="/Applications/MAMP/htdocs/LetsEngineer/curriculum/PHPjob(MANP)/4-2/1599315827_logo.png"></imag>
+</body>
+</html>
+
+
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+       <div class="header">
+           <imag class="left" src="1599315827_logo.png">
+           <div class="right">
+               <p class="above">ようこそさん</p>
+               <p class="under">最終ログイン日：</p>
+           </div>
+       </div>
+       <div class="main clearfix">
+           <div class="up"></div>
+           <div class="down"></div>
+       </div>
+       <p class="footer">Y&I group,inc</p>
+       </div>
+    </body>
+</html>
