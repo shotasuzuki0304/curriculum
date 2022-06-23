acf9294ded45a1f7ad3512c9ebe3561f7b28dbcf
shotasuzuki0304 <104036883+shotasuzuki0304@users.noreply.github.com>
2022-06-23 23:32:16 +0900

    鈴木
    4章チェックテスト
    及び
    質問内容をプッシュさせて頂きました
    
    ・参照場所
    4章チェックテスト→/HTML/課題提出用/4-2
    
    質問内容→/HTML/質問用/4-2質問/index-copy.php
    
    です。
    確認お願いいたします。

--- HTML/質問用/4-2質問/index.php
+++ HTML/質問用/4-2質問/index.php
@@ -1,24 +1,28 @@
 <?php
 require_once("getData.php");
+//getData.phpを一度だけ読み込む
+
 $namae = new getData();
 $userdata = $namae->getUserData();
+//$namaeというインスタンスを作成し、$namaeからUserData()関数を実行して得た結果を、$userdataとして定義する
+
 // var_dump($userdata);
 /*
 出力結果
 array(4) { ["id"]=> string(1) "1" ["first_name"]=> string(6) "翔伍" ["last_name"]=> string(6) "隼田" ["last_login"]=> string(19) "2022-06-14 12:39:04" } 
 object(PDOStatement)#3 (1) { ["queryString"]=> string(36) "SELECT * FROM posts ORDER BY id desc" } 0
 */
+
 $name = $userdata["last_name"].$userdata["first_name"];
+/*
+関数getUserData();はデータベースのusersテーブルから1つの値を取ってくるというもの
+実行内容：$getusers_sql = "SELECT * FROM users limit 1";
+今回使いたいのはlast_nameとfirst_nameなのでそれを抽出し変数$namaeに格納する
+*/
 
 $table = $namae->getPostData();
-var_dump(implode ("," $table));
+//getPostData()に定義されているもう一つの関数を使いたいだけなので、同じインスタンスを使い回す
 
-
-
-
-foreach ($table as $key =>$value) {
-    var_dump($table);
-}
 ?>
 
 
@@ -40,13 +44,13 @@ foreach ($table as $key =>$value) {
        <div class="header clearfix">
            <img class="left" src="1599315827_logo.png">
            <div class="right">
-               <div class="above">ようこそ<?php echo $name?>さん</div>
+               <div class="above">ようこそ <?php echo $name?> さん</div>
                <div class="under">最終ログイン日：<?php echo $userdata["last_login"];?></div>
            </div>
        </div>
 
        
-       <table class="main">
+       <table border class="main">
            <tr>
                <th>記事ID</th>
                <th>タイトル</th>
@@ -54,9 +58,23 @@ foreach ($table as $key =>$value) {
                <th>本文</th>
                <th>投稿日</th>
            </tr>
+           <?php foreach ($table as $key) {?>
            <tr>
-
+              <td><?php echo $key["id"]; ?></td>
+              <td><?php echo $key["title"]; ?></td>
+              <td><?php 
+              $category = $key["category_no"];
+              if ($category == 1) {
+                  echo "食事";
+              } elseif ($category == 2) {
+                  echo "旅行";
+              } else {
+                  echo "その他";
+              }?></td>
+              <td><?php echo $key["comment"]; ?></td>
+              <td><?php echo $key["created"]; ?></td>
            </tr>
+           <?php }?>
        </table>
     
 
