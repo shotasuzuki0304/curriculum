bf874c1da31b4bce2c6bd1e9e52655168164fa30
shotasuzuki0304 <104036883+shotasuzuki0304@users.noreply.github.com>
2022-07-11 13:27:49 +0900

    削除
    一旦全部外に退避させる

--- HTML/課題提出用/PHP/challenge/result-form.php
+++
@@ -1,9 +0,0 @@
-<?php
-$my_name = $_POST['my_name'];
-$e_mail = $_POST['e_mail'];
-$password = $_POST['password'];
-?>
-<p>私の名前は、<?php echo $my_name; ?></p>
-<p>私のメールアドレスは、<?php echo $e_mail; ?></p>
-<p>私のパスワードは、<?php echo $password; ?></p>
-
